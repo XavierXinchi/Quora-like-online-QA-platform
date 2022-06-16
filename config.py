3442e78f866367d0c9397fe82eb8166e0eb2f5f8
XavierXinchi <Xinchi.Shi20@gmail.com>
2022-06-16 17:21:26 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,21 @@
+# 数据库的配置变量
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'zlckqa'
+USERNAME = 'root'
+PASSWORD = 'SXc2002627SXc'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = "156DW5E1DS2D5"
+
+# 邮箱配置
+# 我们项目中用的是QQ邮箱
+MAIL_SERVER = "smtp.qq.com"
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_DEBUG = True
+MAIL_USERNAME = "2574530480@qq.com"
+MAIL_PASSWORD = "wdwajrfikmzoecgd"
+MAIL_DEFAULT_SENDER = "2574530480@qq.com"
